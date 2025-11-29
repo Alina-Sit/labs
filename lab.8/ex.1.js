100f0fdd6a89ef6bb1605f46f47553bf5e7f19ec
AS <alisejene@gmail.com>
2025-11-29 13:32:26 +0200

    Refactor iterate function and add usage example
    Refactor iterate function formatting and add example usage.

--- lab.8/ex.1.js
+++ lab.8/ex.1.js
@@ -1,11 +1,12 @@
-'use strict';
-
-const iterate = (obj, callback) => {
-  const keys = Object.keys(obj);
-  for (const key of keys) {
-    const value = obj[key];
-    callback(key, value, obj);
-  }
+const iterate = (obj, callback) =>{
+    const keys = Object.keys(obj);
+    for (const key of keys){
+        const value = obj[key];
+        callback(key, value, obj);
+    }
 };
 
-module.exports = { iterate };
+const obj = { a: 1, b: 2, c: 3 };
+iterate(obj, (key, value) => {
+  console.log({ key, value });
+});
